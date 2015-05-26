ffa18f875bd9714cca93933493738a86cdc61e93
hamednourhani <info@itstar.ir>
2015-05-26 16:46:06 +0430

    some change

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,27 +1,67 @@
+# require 'rails_helper'
+
+# RSpec.describe PagesController, type: :controller do
+
+#   describe "GET #home" do
+#     it "returns http success" do
+#       get :home
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+#   describe "GET #contact" do
+#     it "returns http success" do
+#       get :contact
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+#   describe "GET #about" do
+#     it "returns http success" do
+#       get :about
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+
+# end
+
 require 'rails_helper'
-
 RSpec.describe PagesController, type: :controller do
-
-  describe "GET #home" do
-    it "returns http success" do
-      get :home
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #contact" do
-    it "returns http success" do
-      get :contact
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #about" do
-    it "returns http success" do
-      get :about
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+render_views
+describe "GET 'home'" do
+it "should be successful" do
+get 'home'
+response.should be_success
+end
+it "should have the right title" do
+get 'home'
+response.should have_selector("title",
+:content => "Ruby on Rails Tutorial Sample App | Home")
+end
+end
+describe "GET 'contact'" do
+it "should be successful" do
+get 'contact'
+response.should be_success
+end
+it "should have the right title" do
+get 'contact'
+response.should have_selector("title",
+:content =>
+"Ruby on Rails Tutorial Sample App | Contact")
+end
+end
+describe "GET 'about'" do
+it "should be successful" do
+get 'about'
+response.should be_success
+end
+it "should have the right title" do
+get 'about'
+response.should have_selector("title",
+:content =>
+"Ruby on Rails Tutorial Sample App | About")
+end
+end
 end
 
