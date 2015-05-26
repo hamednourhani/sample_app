5439a83191da88447834c512aed01dac8536bc8e
hamednourhani <info@itstar.ir>
2015-05-26 17:45:58 +0430

    tester fixed

--- Gemfile
+++ Gemfile
@@ -2,11 +2,14 @@ source 'http://rubygems.org'
 gem 'rails', '4.2.0'
 gem 'sqlite3-ruby', '1.3.3', :require => 'sqlite3'
 gem 'rspec-autotest'
+
 group :development do
-gem 'rspec-rails', '3.2.0'
+	gem 'rspec-rails', '3.2.0'
 end
+
 group :test do
-gem 'rspec', '3.2.0'
-gem 'webrat', '0.7.3'
+	gem 'rspec', '3.2.0'
+	gem 'webrat', '0.7.3'
 end
+
 gem 'autotest-rails', :group => [:development, :test]
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -26,42 +26,57 @@
 # end
 
 require 'rails_helper'
-RSpec.describe PagesController, type: :controller do
-render_views
-describe "GET 'home'" do
-it "should be successful" do
-get 'home'
-response.should be_success
-end
-it "should have the right title" do
-get 'home'
-response.should have_selector("title",
-:content => "Ruby on Rails Tutorial Sample App | Home")
-end
-end
-describe "GET 'contact'" do
-it "should be successful" do
-get 'contact'
-response.should be_success
-end
-it "should have the right title" do
-get 'contact'
-response.should have_selector("title",
-:content =>
-"Ruby on Rails Tutorial Sample App | Contact")
-end
-end
-describe "GET 'about'" do
-it "should be successful" do
-get 'about'
-response.should be_success
-end
-it "should have the right title" do
-get 'about'
-response.should have_selector("title",
-:content =>
-"Ruby on Rails Tutorial Sample App | About")
-end
-end
+RSpec.describe PagesController, type: :controller do 
+  
+
+  render_views
+  
+  describe "GET 'home'" do
+    
+    it "should be successful" do
+      get 'home'
+      expect(response).to be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      expect(response).to have_selector("title",
+      :content => "| Home")
+    end
+
+  end
+  
+  describe "GET 'contact'" do
+    
+    it "should be successful" do
+      get 'contact'
+      expect(response).to be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      expect(response).to have_selector("title",
+      :content =>
+      "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  
+  end
+
+  describe "GET 'about'" do
+  
+    it "should be successful" do
+      get 'about'
+      expect(response).to be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      expect(response).to have_selector("title",
+      :content =>
+      "Ruby on Rails Tutorial Sample App | About")
+    end
+
+  end
+
 end
 
