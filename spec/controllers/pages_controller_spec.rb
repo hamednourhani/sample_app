deb1590c6bda8ed68ecb19c61d1b6ffb668d04af
hamednourhani <info@itstar.ir>
2015-05-30 14:44:55 +0430

    routing test added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,75 +1,56 @@
-# require 'rails_helper'
-
-# RSpec.describe PagesController, type: :controller do
-
-#   describe "GET #home" do
-#     it "returns http success" do
-#       get :home
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
-#   describe "GET #contact" do
-#     it "returns http success" do
-#       get :contact
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
-#   describe "GET #about" do
-#     it "returns http success" do
-#       get :about
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
-# end
 
 require 'rails_helper'
 
-RSpec.describe PagesController, type: :controller do 
+RSpec.describe PagesController, :type => :controller do 
   
 
   render_views
   
   describe "GET 'home'" do
     
-    it "should be successful" do
-      get 'home'
-      expect(response).to be_success
-    end
-    
+        
     it "should have the right title" do
-      get 'home'
-      expect(response).to hav_tag("title")
+      get :home
+      expect(response).to be_success
+      expect(response).to render_template("home")
+      expect(response.body).to include("Home</title>")
+      expect(controller.instance_variable_get(:@title)).to eql("Home")
     end
 
   end
   
+  describe "GET 'about'" do
+    
+    it "should have the right title" do
+      get :about
+      expect(response).to be_success
+      expect(response).to render_template("about")
+      expect(response.body).to include("About</title>")
+      expect(controller.instance_variable_get(:@title)).to eql("About")
+    end
+
+  end
+
   describe "GET 'contact'" do
     
-    it "should be successful" do
-      get 'contact'
-      expect(response).to be_success
-    end
-    
     it "should have the right title" do
-      get 'contact'
-      expect(renderd).to have_tag("title")
+      get :contact
+      expect(response).to be_success
+      expect(response).to render_template("contact")
+      expect(response.body).to include("Contact</title>")
+      expect(controller.instance_variable_get(:@title)).to eql("Contact")
     end
-  
+
   end
 
-  describe "GET 'about'" do
-  
-    it "should be successful" do
-      get 'about'
-      expect(response).to be_success
-    end
-
+  describe "GET 'help'" do
+    
     it "should have the right title" do
-      get 'about'
-      expect(response).to have_selector("title")
+      get :help
+      expect(response).to be_success
+      expect(response).to render_template("help")
+      expect(response.body).to include("Help</title>")
+      expect(controller.instance_variable_get(:@title)).to eql("Help")
     end
 
   end
