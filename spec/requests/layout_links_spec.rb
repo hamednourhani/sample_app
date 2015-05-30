90c090cb255ddb45e93f84130bb2a7dda3ef53d0
hamednourhani <info@itstar.ir>
2015-05-30 13:45:47 +0430

    routing test added

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+RSpec.describe "sampleApp Router", :type => :routing do
+  
+ 	it "should have a Home page at '/'" do
+		expect(:get => "/").to route_to(:controller => "pages", :action =>"home")
+	end
+
+	it "should have a Contact page at '/contact'" do
+		expect(:get => "/pages/contact").to route_to(:controller => "pages", :action =>"contact")
+	end
+	
+	it "should have an About page at '/about'" do
+		expect(:get => "/about").to route_to(:controller => "pages", :action =>"about")
+	end
+	
+	it "should have a Help page at '/help'" do
+		expect(:get => "/help").to route_to(:controller => "pages", :action =>"help")
+	end
+end
+
+
